148dc864e1110b489dbcb578573be1ed357bc501
zxsmile <zx2273284193@dingtalk.com>
2020-03-12 09:58:42 +0800

    add es6模块的加载

--- 练习整理/es6整理/es6模块的加载.js
+++ 练习整理/es6整理/es6模块的加载.js
@@ -74,13 +74,149 @@
 
        (3)exports字段
 
-       
+            - exports的优先级高于main,它有很多用法：
+
+                 (1)子目录别名
+
+                     - package.json文件的exports字段可以指定脚本或子目录的别名。
+
+                            // ./node_modules/es-module-package/package.json
+                            {
+                            "exports": {
+                                "./submodule": "./src/submodule.js"
+                            }
+                            }
+                            
+                            上面的代码指定src/submodule.js别名为submodule，然后就可以从别名加载这个文件。
+
+                            import submodule from 'es-module-package/submodule';
+                            
+                            // 加载 ./node_modules/es-module-package/src/submodule.js 
+
+                    - 如果没有指定别名，就不能用“模块+脚本名”这种形式加载脚本。
+
+                            // 报错
+                            import submodule from 'es-module-package/private-module.js';
+
+                            // 不报错
+                            import submodule from './node_modules/es-module-package/private-module.js';
+
+                 (2)main的别名
+
+                    - exports字段的别名如果是.，就代表模块的主入口，优先级高于main字段，并且可以直接简写成exports字段的值。
+
+                            {
+                            "exports": {
+                                ".": "./main.js"
+                            }
+                            }
+
+                            // 等同于
+                            {
+                            "exports": "./main.js"
+                            }
+
+                    - 由于exports字段只有支持 ES6 的 Node.js 才认识，所以可以用来兼容旧版本的 Node.js。
+
+                            {
+                            "main": "./main-legacy.cjs",
+                            "exports": {
+                                ".": "./main-modern.cjs"
+                            }
+                            }
+                            上面代码中，老版本的 Node.js （不支持 ES6 模块）的入口文件是main-legacy.cjs，新版本的 Node.js 的入口文件是main-modern.cjs。
+
+                 (3)条件加载
+                 
+                    - 利用.这个别名，可以为 ES6 模块和 CommonJS 指定不同的入口。目前，这个功能需要在 Node.js 运行的时候，打开--experimental-conditional-exports标志。
+
+                            {
+                            "type": "module",
+                            "exports": {
+                                ".": {
+                                "require": "./main.cjs",
+                                "default": "./main.js"
+                                }
+                            }
+                            }
+                            上面代码中，别名.的require条件指定require()命令的入口文件（即 CommonJS 的入口），default条件指定其他情况的入口（即 ES6 的入口）
 
 
+                    - 上面的写法可以简写如下。
+
+                            {
+                            "exports": {
+                                "require": "./main.cjs",
+                                "default": "./main.js"
+                            }
+                            }
+                            
+                            
+                    - 注意，如果同时还有其他别名，就不能采用简写，否则或报错。
+
+                            {
+                            // 报错
+                            "exports": {
+                                "./feature": "./lib/feature.js",
+                                "require": "./main.cjs",
+                                "default": "./main.js"
+                            }
+                            }
 
 
+    3. 循环加载
+
+          - ES6 处理“循环加载”与 CommonJS 有本质的不同。ES6 模块是动态引用，如果使用import从一个模块加载变量（即import foo from 'foo'），那些变量不会被缓存，而是成为一个指向被加载模块的引用，需要开发者自己保证，真正取值的时候能够取到值
+
+                 // a.mjs
+                import {bar} from './b';
+                console.log('a.mjs');
+                console.log(bar);
+                export let foo = 'foo';
+
+                // b.mjs
+                import {foo} from './a';
+                console.log('b.mjs');
+                console.log(foo);
+                export let bar = 'bar';
 
 
+                上面代码中，a.mjs加载b.mjs，b.mjs又加载a.mjs，构成循环加载。执行a.mjs，结果如下。
+
+                    $ node --experimental-modules a.mjs
+                    b.mjs
+                    ReferenceError: foo is not defined
+
+                首先，执行a.mjs以后，引擎发现它加载了b.mjs，因此会优先执行b.mjs，然后再执行a.mjs
+                接着，执行b.mjs的时候，已知它从a.mjs输入了foo接口，这时不会去执行a.mjs，而是认为这个接口已经存在了，继续往下执行
+                执行到第三行console.log(foo)的时候，才发现这个接口根本没定义，因此报错
+
+                解决这个问题的方法，就是让b.mjs运行的时候，foo已经有定义了。这可以通过将foo写成函数来解决
+
+                  
+                     // a.mjs
+                    import {bar} from './b';
+                    console.log('a.mjs');
+                    console.log(bar());
+                    function foo() { return 'foo' }
+                    export {foo};
+
+                    // b.mjs
+                    import {foo} from './a';
+                    console.log('b.mjs');
+                    console.log(foo());
+                    function bar() { return 'bar' }
+                    export {bar};
+                    这时再执行a.mjs就可以得到预期结果。
+
+                    $ node --experimental-modules a.mjs
+                    b.mjs
+                    foo
+                    a.mjs
+                    bar
+
+                    这是因为函数具有提升作用，在执行import {bar} from './b'时，函数foo就已经有定义了，所以b.mjs加载的时候不会报错
+                    这也意味着，如果把函数foo改写成函数表达式，也会报错
 
 
 */
