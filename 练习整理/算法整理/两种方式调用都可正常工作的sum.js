5b15185a104c3076e4309519050c63ac4c82cc56
zxsmile <zx2273284193@dingtalk.com>
2020-04-06 16:17:27 +0800

    add 函数柯里化

--- 练习整理/算法整理/两种方式调用都可正常工作的sum.js
+++ 练习整理/算法整理/两种方式调用都可正常工作的sum.js
@@ -17,7 +17,6 @@ function sum(...a){
 
         return s
     }else{
-        console.log(typeof [...a][0])
         var s=0
         for(var i=0;i<arguments.length;i++){
              s = s+arguments[i]
@@ -27,6 +26,34 @@ function sum(...a){
     
 }
 
+
+
 console.log(sum(1)(2)(3)(4))
 console.log(sum(1,2,3,4))
 
+
+//可三种方法调用
+
+function add() {
+    // 第一次执行时，定义一个数组专门用来存储所有的参数
+    var _args = Array.from(arguments);
+
+    // 在内部声明一个函数，利用闭包的特性保存_args并收集所有的参数值
+    var _adder = function() {
+        _args.push(...arguments);
+        return _adder;
+    };
+
+    // 利用toString隐式转换的特性，当最后执行时隐式转换，并计算最终的值返回
+    _adder.toString = function () {
+        return _args.reduce(function (a, b) {
+            return a + b;
+        });
+    }
+    return _adder;
+}
+
+
+alert(add(1)(2))
+alert(add(1,2))
+alert(add(1,2)(2))
