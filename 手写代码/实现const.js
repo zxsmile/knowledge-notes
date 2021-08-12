3d7f7e7d047926f72d6840f59fdf592bdb366dfc
zxsmile <2273284193@qq.com>
2021-08-12 19:05:53 +0800

    add 实现const

---
+++ 手写代码/实现const.js
@@ -0,0 +1,37 @@
+function newConst(key,val){
+    let dsc={
+        value:val,
+        writable:false
+    }
+
+    Object.defineProperty(window,key,dsc)
+
+}
+
+newConst('a',3)
+a=6
+console.log(a) //3
+
+
+
+function newConst(key,val){
+    window.key = val
+    let dsc={
+        get:()=>{
+          return val
+        },
+        set:(value)=>{
+          throw '不能改写'
+        }
+    }
+
+    Object.defineProperty(window,key,dsc)
+
+}
+newConst('a',3)
+a=6
+console.log(a) 
+
+//报错
+
+//注：value或者writable不能和set和get同时出现
