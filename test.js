14e13324de4d21e98f682d99506696e8b45737f3
zxsmile <2273284193@qq.com>
2021-08-16 19:24:47 +0800

    add 红宝书

---
+++ test.js
@@ -0,0 +1,17 @@
+let obj = {name:"aa"}
+function fn(name){
+    
+    console.log(name)
+    var name =10
+    function name(){}
+    console.log(name)
+}
+//fn('ll')
+//console.log(name)
+//fn.apply(obj)
+let a = {}
+console.log(a.valueOf())
+console.log(a.toString())
+console.log(typeof a)
+console.log(Number(null))
+//console.log(a.toString())
