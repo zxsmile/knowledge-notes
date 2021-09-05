bed62bf19600803abb4f972aafd443f4fd23afe6
zxsmile <2273284193@qq.com>
2021-09-05 20:36:38 +0800

    add js

--- test.js
+++ test.js
@@ -1,59 +1,10 @@
-// async function async1(){
-//     await async2()
-//     console.log('async1 end')
-// } 
-
-// function async2(){
-//     console.log('async2 end')
-// }
-// async1()
-
-// setTimeout(()=>{
-//    console.log('setTimeout')
-// },0)
-
-// new Promise(reslove=>{
-//     reslove()
-// }).then(()=>{
-//     console.log('promise')
-// })
-class Subject{
-    constructor() {
-      this.subs = []
-    }
-
-    add(sub) {
-        this.subs.push(sub)
-    }
-
-    remove(sub) {
-        this.subs.filter(item=>{
-            return item!==sub
-        })
-    }
-    
-    notify() {
-        this.subs.forEach(sub=>{
-            sub.update.apply(sub)
-        })
-    }
-}
-
-class Observer{
-    constructor(name) {
-        this.name = name
-    }
-    update() {
-        console.log(this.name)
-    }
-}
-
-let sub = new Subject()
-let obj1 = new Observer('小明')
-let obj2 = new Observer('小红')
-obj2.update=function(){
-    console.log('666')
-}
-sub.add(obj1)
-sub.add(obj2)
-sub.notify()
+str = '<h1>我买了一个{{thing}}，好{{mood}}啊</h1>'
+let re = /{{([^}}]+)?}}/g
+      let match = re.exec(str)
+    //   match:[
+	// 	    "{{thing}}",
+	// 	    " thing", 
+	// 	    index: 21,
+	// 	    input: "<p>Hello, my name is <%name%>. I\'m <%age%> years old.</p>"
+		//]
+console.log(match)
