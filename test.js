f3756e8514a389a57951e2a2c8b176e1eae4c017
zxsmile <2273284193@qq.com>
2021-09-16 19:55:55 +0800

    add js

--- test.js
+++ test.js
@@ -1,3 +1,79 @@
-let a = true
-console.log(a.toString())
-console.log(Object.prototype.toString.call(new document()))
+function deepCopy(obj,cache=new Map()){
+    if(cache.get(obj)){
+        return cache.get(obj)
+    }
+
+    if(obj instanceof Object){
+       let newObj = {}
+       if(obj instanceof Array){
+         newObj = []
+
+       }else if(obj instanceof Function){
+         newObj = function(){
+             return obj.call(this,...arguments)
+         }
+
+       }else if(obj instanceof RegExp){
+         newObj = new RegExp(obj.source,obj.flags)
+
+       }else if(obj instanceof Date){
+           newObj = new Date(obj)
+
+       }else{
+           newObj = {}
+       }
+       cache.set(obj,newObj)
+       console.log(cache)
+       for(var key in obj){
+           if(obj.hasOwnProperty(key)){
+               newObj[key]=deepCopy(obj[key],cache)
+           }
+       }
+
+       return newObj
+    }else{
+        return obj
+    }
+}
+
+let obj = {
+    a:1,
+    b:{s:2}
+}
+deepCopy(obj)
+// const a = {
+//     i: {obj:{k:5}},
+//     s: "",
+//     bool: false,
+//     n: null,
+//     u: undefined,
+//     sym: Symbol(),
+//     obj: {
+//       i: 6,
+//       s: "",
+//       bool: false,
+//       n: null,
+//       u: undefined,
+//       sym: Symbol(),
+//     },
+//     array: [
+//       {
+//         nan: NaN,
+//         i: Infinity,
+//         s: "",
+//         bool: false,
+//         n: null,
+//         u: undefined,
+//         sym: Symbol(),
+//       },
+//       123,
+//     ],
+//     fn: function () {
+//       return "fn";
+//     },
+//     date: new Date(),
+//     re: /hi\d/gi,
+//   };
+//   let a2 = deepCopy(a);
+
+  //console.log(a2)
