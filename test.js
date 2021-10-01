204fa65655c1bb8ad3a91fe0002c9aad0495caea
zxlalalala <zx2273284193@dingtalk.com>
2021-10-01 12:02:00 +0800

    add vue

--- test.js
+++ test.js
@@ -1,25 +1,43 @@
+class Vue{
+  constructor(options) {
+     this.data = options.data
+     this.observe(this.data,options.render)
+  }
+  observe(obj,render) {
+    Object.keys(obj).forEach(key => {
+      this.defineReactive(obj,key,obj[key],render) 
+    })
+  }
 
-let res = {}
-let s=''
-function dp(obj,newKey){
-       for(var key in obj){
-           if(Object.prototype.toString.call(obj[key]) ==='[object Object]'){
-               if(newKey){
-                   dp(obj[key],`${newKey}${key}.`)
-               }else{
-                   dp(obj[key],`${key}.`)
-               }
-               
-           }else{
-               if(newKey){
-                   res[newKey+key]=obj[key]
-               }else{
-                   res[key]=obj[key]
-               }
-           }
+  defineReactive(obj,key,value,render) {
+    let that = this
+     Object.defineProperty(that,key,{
+       configurable:true,
+      // writable:true,
+       enumerable:true,
+       get:() =>{
+         return that.data[key]
+       },
+       set:newValue => {
+        that.data[key] = newValue
+         render(that.data[key])
        }
-       
+     })
+  }
+  proxy(obj) {
+     
+  }
 }
 
-dp({name:{first:{ss:5,dd:6}},age:{hh:5}},null)
-console.log(res)
+let app = new Vue({
+  data:{
+    text1:1,
+    text2:2,
+    text3:3
+  },
+  render(data) {
+    console.log(data)
+  }
+})
+
+app.text1 = 6
