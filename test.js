00d841fdf970694147edf33f2ffcfdb7db64fb5d
zxlalalala <zx2273284193@dingtalk.com>
2021-10-14 20:47:59 +0800

    update

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
-console.log(parseFloat(12000000000).toLocaleString())
+Function.prototype.mybind = function(newThis){
+  
+   let arg = Array.from(arguments).slice(1)
+   let self = this
+   let bount = function(){
+      let newArg = arg.concat(Array.from(arguments))
+      if(this instanceof bount){
+         let res = self.apply(this,newArg)
+
+      }else{
+        return self.apply(newThis,newArg)
+      }
+   }
+
+   if(self.prototype){
+     
+   }
+}
