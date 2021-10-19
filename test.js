dfc1971050526f0a1720084f4a49fe20d138fca3
zxlalalala <zx2273284193@dingtalk.com>
2021-10-19 17:41:59 +0800

    add 手写代码

--- test.js
+++ test.js
@@ -1,63 +1,72 @@
-// 1.参数字符串，布尔，数值会报错
-// 2.参数为undefined，null，除了arguments的对象会返回undefined
-// 3.所以用arguments的属性callee（返回正在执行的函数）来区分arguments对象和别的对象
+class MyPromise{
+   constructor(handle) {
+       this.promiseState = 'pending'
+       this.promiseResult = null
+       this.onFulfilledCallbacks = []
+       this.onRejectedCallbacks = []
+       try{
+          this.handle(this.resolve.bind(this),this.reject.bind(this))
+       }catch(e){
+          this.reject(e)
+       }
+       
+   }
 
-Function.prototype.myApply = function(newThis,argArr){
-	//this指的是调用apply的函数
-    if(typeof this !== 'function'){
-        throw new TypeError(this + ' is not a function');
-    }
-    newThis = newThis ? Object(newThis) : window
-	//newThis.fn = this
-	//使用Symbol()
-  let fn = Symbol()
-  newThis[fn] = this
-	//使用缓存
-	// let originalVal = newThis.fn;
-    // let hasOriginalVal = newThis.hasOwnProperty(fn);
-	// newThis.fn = this
-	
-	let result
-        
-    if(argArr){
-		let errArr = ['string','boolean','number']
-		let argRes = typeof argArr
-		if(errArr.includes(argRes)){
-			throw '参数有误'
-		}
-		let objErrArr = ['undefined','null','function']
-		if((argRes==='object'&& !argArr.callee) || objErrArr.includes(argRes)){
-			return undefined
-		}
-		//使用symbol()
-		result = newThis[fn](...argArr)
-		//使用缓存
-		//result = newThis.fn(...argArr)
-	}else{
-		//使用symbol()
-		result = newThis[fn]()
-		//使用缓存
-		//result = newThis.fn()
-	}
-	//使用symbol()
-	delete newThis[fn]
-	//使用缓存
-	//delete newThis.fn
-	// if(hasOriginalVal){
-    //     newThis.fn = originalVal
-    // }
-	return result     
+   resolve(value) {
+      if(value instanceof MyPromise){
+         return value.then(this.resolve,this.reject)
+      }
+
+      setTimeout(() => {
+         if(this.promiseState !== 'pending'){
+            return
+         }
+         this.promiseState = 'fulfilled'
+         this.promiseResult = value
+         while(this.onFulfilledCallbacks.length){
+            this.onFulfilledCallbacks.shift(this.promiseResult)
+         }
+      })
+   }
+
+   reject(reason) {
+      setTimeout(() => {
+         if(this.promiseState !== 'pending'){
+            return
+         }
+         this.promiseState = 'rejected'
+         this.promiseResult = reason
+         while(this.onRejectedCallbacks.length){
+            this.onRejectedCallbacks.shift(this.promiseResult)
+         }
+      })
+   }
+
+   then(onFulfilled,onRejected){
+      onFullfilled = typeof onFulfilled === 'function' ? onFulfilled : val => val
+      onRejected = typeof onRejected === 'function' ? onRejected : reason => {throw reason}
+      return new MyPromise((onFulfilledNext,onRejectedNext) => {
+         let resultPromise =  lastThen => {
+            try{
+              let x = lastThen(this.promiseResult)
+              if(x instanceof MyPromise){
+                 x.then(onFulfilledNext,onRejectedNext)
+              }else{
+               onFulfilledNext(x)
+              }
+            }catch(e){
+               onRejectedNext(e)
+            }
+         }
+
+         if(this.promiseState === 'fulfilled'){
+            resultPromise(onFulfilled)
+         }else if(this.promiseState === 'rejected'){
+            resultPromise(onRejected)
+         }else if(this.promiseState === 'pending'){
+            this.onFulfilledCallbacks.push(resultPromise.bind(this,onFulfilled))
+            this.onRejectedCallbacks.push(resultPromise.bind(this,onRejected))
+         }
+      })
+   }
 }
-
-let obj = {
-    name:'milk',
-    fn:5
-  }
-  
-  function bar(){
-    console.log(this.name)
-    return 6
-  }
-  
-  console.log(bar.apply(obj,undefined))
-  //console.log(obj.fn)
