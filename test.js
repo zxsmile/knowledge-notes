9a2a546bb4c9a4e47c115008631df40391d9ab6e
zxlalalala <zx2273284193@dingtalk.com>
2021-10-20 14:25:25 +0800

    add 手写代码

--- test.js
+++ test.js
@@ -1,17 +1,1 @@
 
-function flatter(arr){
-   let res = []
-  for(var i=0;i<arr.length;i++){
-     if(Array.isArray(arr[i])){
-        res = res.concat(flatter(arr[i]))
-     }else{
-        res.push(arr[i])
-     }
-  }
-  return res
-}
-
-
-
-let arr = [1,[2,3],[5,[6]]]
-console.log(flatter(arr))
