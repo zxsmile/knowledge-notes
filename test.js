664c77e9f17e5c699bd32942a8ca54e15aeaa279
zxlalalala <zx2273284193@dingtalk.com>
2021-11-02 13:03:20 +0800

    add css

--- test.js
+++ test.js
@@ -1,17 +1,1 @@
-let arr = []
-function fn(arr){
-    let result = []
-  for(var i=0;i<arr.length;i++){
-     let newArr = arr.slice(i+1)
-     let max = newArr.sort((a,b)=>{
-        return b-a
-     })[0]
-      result.push(max-arr[i])
-  }
-
-  result.sort((a,b) => {
-     return b-a
-  })
-
-  return result[0]
-}
+console.log('AB'.charCodeAt('B'))
