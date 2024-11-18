bc558e7d15404e2f3861232d922e1daeae47789c
zxlalalala <zx2273284193@dingtalk.com>
2024-11-18 17:52:20 +0800

    update

--- test.js
+++ test.js
@@ -1,260 +1,208 @@
-class MyClass{
+new Vue({
+    router,
+    store,
+    render:h=>h(App)
+}).$mount('#app')
 
-  constructor(handle){
-      if(typeof handle !== 'function'){
-          throw new Error('error')
-      }
-      this.status = 'pending'
-      this.value = null
-      this.reason = null
-      this.onFulfilledCallback = []
-      this.onRejectedCallback = []
-      try{
-        handle(this.resolve,this.reject)
-      }catch(e){
-        this.reject(e)
-      }
-  
-  }
-
-  resolve = (value) => {
-    if(this.status === 'pending'){
-        this.status = 'fulfilled'
-        this.value = value
-        while(this.onFulfilledCallback.length){
-            this.onFulfilledCallback.shift()(this.value)
-        }
-    }
-  }
-
-  reject = (reason) => {
-    if(this.status === 'pending'){
-        this.status = 'rejected'
-        this.reason = reason
-        while(this.onRejectedCallback.length){
-            this.onRejectedCallback.shift()(this.reason)
-        }
-    }
-  }
-
-  then(onFulfilled,onRejected) {
-    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value
-    onRejected = typeof onRejected === 'function' ? onRejected : reason => {throw reason}
-    let thenPromise = new MyPromise((resolve,reject)=> {
-         if(this.status === 'fulfilled'){
-             queueMicrotask(() => {
-                 try{
-                   let res = onFulfilled(this.value)
-                   this.resolvePromise(thenPromise,res,resolve,reject)
-                 }catch(e){
-                     reject(e)
-                 }
-             })
-         }else if(this.status === 'rejected'){
-            queueMicrotask(() => {
-                try{
-                  let res = onRejected(this.reason)
-                  this.resolvePromise(thenPromise,res,resolve,reject)
-                }catch(e){
-                    reject(e)
-                }
-            })
-        }else if(this.status === 'pending'){
-            this.onFulfilledCallback.push( 
-                () => {
-                  queueMicrotask(() => {
-                    try{
-                      let res = onFulfilled(this.value)
-                      this.resolvePromise(thenPromise,res,resolve,reject)
-                    }catch(e){
-                        reject(e)
-                    }
-                  })  
-                }
-                
-            )
-
-            this.onRejectedCallback.push( 
-                () => {
-                 queueMicrotask(() => {
-                    try{
-                    let res = onRejected(this.reason)
-                    this.resolvePromise(thenPromise,res,resolve,reject)
-                    }catch(e){
-                        reject(e)
-                    }
-                 }) 
-                }
-            )
-
-           
-        }
-    })
-    return thenPromise
-  }
-
-  resolvePromise(thenPromise,x,resolve,reject) {
-      if(thenPromise === x){
-          throw error('error')
-      }
-      
-      if(x instanceof MyPromise){
-        x.then((y)=> {
-            this.resolvePromise(thenPromise,y,resolve,reject)
-        },e=>{
-            reject(e)
-        })
-      }
-
-      if((typeof x === 'object' || typeof x === 'function') && x !== null){
-         
-          try{
-
-           var then = x.then
-
-          }catch(e){
-
-              reject(e)
-
-          }
-
-          if(typeof then === 'function'){
-            let called = false
-            try{
-                then.call(x,y=>{
-                    if(called){
-                        return
-                    }
-                    called = true
-                    this.resolvePromise(thenPromise,y,resolve,reject)
-                 },e=>{
-                     if(called){
-                         return
-                     }
-                     called = true
-                     reject(e)
-                 })
-            }catch(e){
-                if(called){
-                    return
-                }
-                called = true
-                reject(e)
-            }
-           
-          }else{
-              resolve
-          }
-
-      }else{
-          resolve(x)
-      }   
-
-  }
-
-  static resolve(value) {
-    if(value instanceof MyPromise){
-        return value
-    }
-
-    return new MyPromise((reslove,reject)=>{
-        reslove(value)
-    })
-  }
-
-  static reject(reason) {
-    return new MyPromise((reslove,reject)=>{
-        reject(reason)
-    })
-  }
-
-  static all(list) {
-      return new MyPromise((resolve,reject) => {
-          if(!Array.isArray(list)){
-              throw new TypeError('参数必须是数组')
-          }
-
-          let resArr = []
-          let count = 0
-          for(let i =0;i<list.length;i++){
-              let item = list[i]
-              this.resolve(item).then((val) => {
-                resArr[i] = val
-                count++
-                if(count === list.length){
-                    resolve(resArr)
-                }
-              },e=>{
-                reject(e)
-              })
-          }
-      })
-  }
-
-  static race(list) {
-    return MyPromise((resolve,reject) => {
-        if(!Array.isArray(list)){
-            throw new TypeError('参数必须是数组')
-        }
-        for(let i=0;i<list.length;i++){
-            let item = list[i]
-            this.resolve(item).then(val=>{
-                resolve(val)
-            },e=>{
-                reject(e)
-            })
-        }
-    })
-  }
-
-  static allSettled(list) {
-      if(!Array.isArray(list)){
-          throw new TypeError('参数必须是数组')
-      }
-
-      return new MyPromise((resolve,reject) => {
-          let resArr = []
-          let count = 0
-          for(let i=0;i<list.length;i++){
-              let item = list[i]
-              this.reslove(item).then(value => {
-                  resArr[i] = {status:'fulfilled',value:value}
-                  count++
-                  if(count === list.length){
-                    resolve(resArr)
-                  }
-              },e => {
-                  resArr[i] = {status:'rejected',value:e}
-                  if(count === list.length){
-                    resolve(resArr)
-                  }
-              })
-             
-          }
-      })
-  }
-
-  static any(list) {
-      if(!Array.isArray(list)){
-          throw new TypeError('参数必须是数组')
-      }
-
-      return new MyPromise((resolve,reject) => {
-          let resArr = []
-          let count = 0
-          for(let i=0;i<list.length;i++){
-              let item = list[i]
-              this.resolve(item).then(val => {
-                  resolve(val)
-              },err=>{
-                  resArr[count] = err
-                  count++
-                  if(count === list.length){
-                    reject(new AggregateError('No Promise in Promise.any was resolved'))
-                  }
-              })
-          }
-
-      })
-  }
+function Vue(options) {
+    this._init(options)
 }
+
+initMixin(Vue)
+
+function initMixin(Vue) {
+    Vue.prototype._init = function(options){
+      let vm = this
+      vm.$options = options
+      initState(vm)
+    }
+}
+
+function initState(vm) {
+   let opt = vm.$options
+   if(opt.props){
+       initProps(vm)
+   }
+   initSetup(vm)
+   if(opt.methods){
+       initMethod(vm)
+   }
+   if(opt.data){
+       initData(vm)
+   }
+   if(opt.computed){
+       initComputed(vm)
+   }
+   if(opt.watch){
+       initWatch(vm)
+   }
+}
+
+
+new Vue({
+    router,
+    store,
+    render:h => h(App)
+}).$mount('#app')
+
+function Vue(options) {
+  this._init(options)
+}
+
+initMixin(Vue)
+
+function initMixin(Vue){
+    Vue.prototype._init = function(options) {
+        let vm = this
+        vm.$options = options
+        initState(vm)
+    }
+}
+
+function initState(vm){
+    let opts = vm.$options
+    if(opts.props){
+        initProps(vm)
+    }
+    initSetup(vm)
+    if(opts.methods){
+        initMethods(vm)
+    }
+
+    if(opts.data){
+        initData(vm)
+    }
+    if(opts.computed){
+        initComputed(vm)
+    }
+    if(opts.watch){
+        initWatch(vm)
+    }
+}
+
+function initData(vm) {
+    let data = vm.$options.data
+    data = vm._data = typeof data === 'function' ? data.call(vm) : data || {}
+
+    if(Object.prototype.toString.call(data) !== '[object Object]'){
+        return new Error('data functions should return an object')
+    }
+
+    let keys = Object.keys(data)
+    for(let key in keys){
+        proxy(vm,'_data',key)
+    }
+
+    observe(data)
+}
+
+function proxy(obj,sourceKey,key){
+   Object.defineProperty(obj,sourceKey,{
+       get() {
+           return obj[sourceKey][key]
+       },
+       set(val) {
+           obj[sourceKey][key] = val
+       }
+   })
+}
+
+let hasOwnPrototype = Object.prototype.hasOwnPrototype
+function hasOwn (obj,key) {
+  return hasOwnPrototype.call(obj,key)
+}
+function observe(data) {
+    if(data && hasOwn(data,'__ob__') && data.__ob__ instanceof Observer){
+        return data.__ob__
+    }
+
+    if(Array.isArray(data) || Object.prototype.toString.call(data) === '[object Object]'){
+        new Observer(data)
+    }
+}
+
+class Observer{
+    constructor(data) {
+        Object.defineProperty(data,'__ob__',{
+            value:this,
+            enumerable:false,
+            writable:true,
+            configurable:true,
+        })
+
+        if(Array.isArray(data)){
+            data.__proto__ = arrayMethods
+            this.observeArray(data);
+        }else{
+            this.walk(data)
+        }
+    }
+    walk(data) {
+        let keys = Object.keys(data)   
+        for(let key in keys){
+            defineReactive(data,key,data[key])
+        } 
+    }
+    observeArray() {
+        data.forEach(item => {
+            observe(item)
+        })
+    }
+}
+
+function defineReactive(obj,key,value) {
+    observe(value)
+    Object.defineProperty(obj,key,{
+        get() {
+            return value
+        },
+
+        set(newVal){
+          if(newVal === value){
+              return 
+          }
+          obj[key] = newVal
+        }
+    })
+}
+
+let arrayProto = Array.prototype
+export default arrayMethods = Object.create(arrayProto)
+
+let methodsPath = [
+    'push',
+    'pop',
+    'unshift',
+    'shift',
+    'splice',
+    'sort',
+    'reserve'
+]
+
+methodsPath.forEach(method => {
+    Object.defineProperty(arrayMethods,item,function mutator(...arg){
+         let result = arrayProto[method].apply(this,arg)
+         let ob = this.__ob__
+         let inserted
+
+         switch(item){
+             case push:
+             case unshift:
+                inserted = arg
+              break;
+
+             case splice: 
+                inserted = arg.slice(2)
+
+              break;
+
+              default:
+              break;
+         }
+
+         if(inserted){
+            ob.observeArray(inserted)
+         }
+         return result
+    })
+})
